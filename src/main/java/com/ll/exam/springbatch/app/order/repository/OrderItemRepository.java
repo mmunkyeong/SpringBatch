c7d8bd4e229bc468b6dcb3980b3e1f699a57c53c
mmunkyeong <jmk4379@naver.com>
2023-06-29 22:21:06 +0900

    feat: orderItem의 id가 6보다 작은 데이터만 정산데이터 생성하도록

--- src/main/java/com/ll/exam/springbatch/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/ll/exam/springbatch/app/order/repository/OrderItemRepository.java
@@ -1,7 +1,10 @@
 package com.ll.exam.springbatch.app.order.repository;
 
 import com.ll.exam.springbatch.app.order.entity.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    Page<OrderItem> findAllByIdLessThan(long id, Pageable pageable);
 }
