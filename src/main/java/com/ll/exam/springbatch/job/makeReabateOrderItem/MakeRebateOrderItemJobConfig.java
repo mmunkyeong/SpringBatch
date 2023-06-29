fcf9dabf828f9c31c1d1bd771c840d87bca80712
mmunkyeong <jmk4379@naver.com>
2023-06-30 00:24:03 +0900

    feat: 날짜 범위

--- src/main/java/com/ll/exam/springbatch/job/makeReabateOrderItem/MakeRebateOrderItemJobConfig.java
+++ src/main/java/com/ll/exam/springbatch/job/makeReabateOrderItem/MakeRebateOrderItemJobConfig.java
@@ -4,6 +4,7 @@ import com.ll.exam.springbatch.app.order.entity.OrderItem;
 import com.ll.exam.springbatch.app.order.entity.RebateOrderItem;
 import com.ll.exam.springbatch.app.order.repository.OrderItemRepository;
 import com.ll.exam.springbatch.app.order.repository.RebateOrderItemRepository;
+import com.ll.exam.springbatch.util.Util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -25,6 +26,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -66,15 +68,18 @@ public class MakeRebateOrderItemJobConfig {
     @StepScope
     @Bean
     public RepositoryItemReader<OrderItem> orderItemReader(
-            @Value("#{jobParameters['fromId']}") long fromId,
-            @Value("#{jobParameters['toId']}") long toId
+            @Value("#{jobParameters['month']}") String yearMonth
     ) {
+        int monthEndDay = Util.date.getEndDayOf(yearMonth);
+        LocalDateTime fromDate = Util.date.parse(yearMonth + "-01 00:00:00.000000");
+        LocalDateTime toDate = Util.date.parse(yearMonth + "-%02d 23:59:59.999999".formatted(monthEndDay));
+
         return new RepositoryItemReaderBuilder<OrderItem>()
                 .name("orderItemReader")
                 .repository(orderItemRepository)
-                .methodName("findAllByIsPaid")
+                .methodName("findAllByPayDateBetween")
                 .pageSize(100)
-                .arguments(Arrays.asList(true))
+                .arguments(Arrays.asList(fromDate, toDate))
                 .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
                 .build();
     }
