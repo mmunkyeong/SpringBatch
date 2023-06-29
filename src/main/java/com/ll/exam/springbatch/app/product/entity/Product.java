6bc11078d80215724387227f6b64c4708ea1888a
mmunkyeong <jmk4379@naver.com>
2023-06-29 14:51:47 +0900

    feat: 테스트 상품 옵션 데이터 생성(각 상품당 4개씩)

--- src/main/java/com/ll/exam/springbatch/app/product/entity/Product.java
+++ src/main/java/com/ll/exam/springbatch/app/product/entity/Product.java
@@ -2,11 +2,17 @@ package com.ll.exam.springbatch.app.product.entity;
 
 import com.ll.exam.springbatch.app.base.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
 
 
 @Entity
@@ -18,4 +24,16 @@ public class Product extends BaseEntity {
     private int price;
     private String name;
     private String makerShopName;
+    private boolean isSoldOut; // 관련 옵션들이 전부 판매불능 상태일 때
+
+    @Builder.Default
+    @OneToMany(mappedBy = "product", cascade = ALL, orphanRemoval = true)
+    private List<ProductOption> productOptions = new ArrayList<>();
+
+    public void addOption(ProductOption option) {
+        option.setProduct(this);
+        option.setPrice(getPrice());
+
+        productOptions.add(option);
+    }
 }
--- src/main/java/com/ll/exam/springbatch/app/product/service/ProductService.java
+++ src/main/java/com/ll/exam/springbatch/app/product/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.ll.exam.springbatch.app.product.service;
 
 import com.ll.exam.springbatch.app.product.entity.Product;
+import com.ll.exam.springbatch.app.product.entity.ProductOption;
 import com.ll.exam.springbatch.app.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,13 +14,18 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public void create(String name, int price, String makerShopName) {
+    public void create(String name, int price, String makerShopName, List<ProductOption> options) {
         Product product = Product.builder()
                 .name(name)
                 .price(price)
                 .makerShopName(makerShopName)
                 .build();
 
+        productRepository.save(product);
+
+        for ( ProductOption option : options ) {
+            product.addOption(option);
+        }
         productRepository.save(product);
     }
 }
