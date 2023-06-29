cd8d463521b46f7b1eb22cf43fa3a4b18902ac47
mmunkyeong <jmk4379@naver.com>
2023-06-29 16:56:58 +0900

    feat: 예치금으로만 결제

--- src/main/java/com/ll/exam/springbatch/app/base/DevInitData.java
+++ src/main/java/com/ll/exam/springbatch/app/base/DevInitData.java
@@ -3,6 +3,7 @@ package com.ll.exam.springbatch.app.base;
 import com.ll.exam.springbatch.app.cart.service.CartService;
 import com.ll.exam.springbatch.app.member.entity.Member;
 import com.ll.exam.springbatch.app.member.service.MemberService;
+import com.ll.exam.springbatch.app.order.entity.Order;
 import com.ll.exam.springbatch.app.order.service.OrderService;
 import com.ll.exam.springbatch.app.product.entity.Product;
 import com.ll.exam.springbatch.app.product.entity.ProductOption;
@@ -37,6 +38,8 @@ public class DevInitData {
             // 5천원 사용
             memberService.addCash(member1, -5_000, "출금__일반");
 
+            // 삼십만원 충전
+            memberService.addCash(member1, 300_000, "충전__무통장입금");
             // 현재 보유중인 캐시 금액
             long restCash = memberService.getRestCash(member1);
 
@@ -50,7 +53,10 @@ public class DevInitData {
             cartService.addItem(member1, productOption__RED_44, 2); // productOption__RED_44 총 수량 3
             cartService.addItem(member1, productOption__BLUE_44, 1); // productOption__BLUE_44 총 수량 1
 
-            orderService.createFromCart(member1);
+            Order order1 = orderService.createFromCart(member1);
+
+            int order1PayPrice = order1.calculatePayPrice();
+            orderService.payByRestCashOnly(order1);
         };
     }
 }
