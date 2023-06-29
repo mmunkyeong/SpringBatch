acbbccfb392457c9f57ff2d7f5d516a681fe76d4
mmunkyeong <jmk4379@naver.com>
2023-06-29 14:32:37 +0900

    feat: Test를 위해 step1만 수행하도록 잠시 주석처리

--- src/main/java/com/ll/exam/springbatch/job/simple/SimpleJobConfig.java
+++ src/main/java/com/ll/exam/springbatch/job/simple/SimpleJobConfig.java
@@ -1,12 +1,12 @@
 package com.ll.exam.springbatch.job.simple;
 
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -17,13 +17,14 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Slf4j
 @Configuration
-public class SimpleJobConfiguration {
+public class SimpleJobConfig {
     @Bean // 기본 싱글톤 : 스프링부트 앱이 꺼지기 전까지 살아있음
-    public Job simpleJob1(JobRepository jobRepository, Step simpleStep1, Step simpleStep2){
+    public Job simpleJob(JobRepository jobRepository, Step simpleStep1){
         return new JobBuilder("simpleJob",jobRepository)
-                .incrementer(new RunIdIncrementer())// 강제로 매번 다른 ID를 실행시에 파라미터로 구현
+                // 파라미터가 다르면 새 명령으로 보기 때문에 밑에 추가한 내용을 잠시 주석 처리
+               // .incrementer(new RunIdIncrementer())// 강제로 매번 다른 ID를 실행시에 파라미터로 구현
                 .start(simpleStep1)
-                .next(simpleStep2) // 위에꺼 다 실행된 후에 실행
+               // .next(simpleStep2) // 위에꺼 다 실행된 후에 실행
                 .build();
     }
 
@@ -42,7 +43,7 @@ public class SimpleJobConfiguration {
             return RepeatStatus.FINISHED;
         });
     }
-
+/*
     @Bean
     @JobScope
     public Step simpleStep2(JobRepository jobRepository, Tasklet testStep2Tasklet, PlatformTransactionManager platformTransactionManager){
@@ -55,7 +56,14 @@ public class SimpleJobConfiguration {
     public Tasklet testStep2Tasklet(){
         return ((contribution, chunkContext) -> {
             System.out.println(">>step2");
+            /*
+            if(false) { // 두번째 스텝이 실패하도록 => 성공하도록(위에 주석처리 후 false로 실행하면 실행 실패한 step2만 다시 실행)
+                throw new Exception("Fail: step2");
+            }
+
             return RepeatStatus.FINISHED;
         });
     }
+    */
+
 }
